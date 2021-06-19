701bea476c1265ab2cb28dbf562d9bfa88e0cd6f
Sunny <gpoisoned@gmail.com>
2021-06-19 13:13:21 -0600

    Initialize repository

---
+++ README.md
@@ -0,0 +1,1 @@
+# NepaliGhar CMS Backend
---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'e7838c49b9937d4643ba09717a60a3e2'),
+    },
+  },
+});
